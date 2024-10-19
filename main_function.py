28966b092ac62c660cdc0ea17fcfab6f77db4e59
aradjoliny <aradjolint2005@gmail.com>
2024-10-19 12:36:38 +0100

    ADDING MAIN FUNCTION

--- main_function.py
+++ main_function.py
@@ -1,1 +1,36 @@
 
+
+def calculate_monthly_income(loan_amount, part_time_income, job_status):
+    if job_status == True:
+        return (loan_amount / 12) + float(part_time_income)
+    else:
+        return (loan_amount / 12)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # Variables
+    job_status = False
+    loan_amount = 0
+    part_time_income = 0
+
+    # Get maintenance/student loan from user
+    print("Hi! Welcome to the best financial simulator game ever!\n")
+    loan_amount = int(input("Lets get started! How much maintenance/student loan do you have per year?\n"))
+    # Get their job status, are they working a part time job?
+    job_status = input("Are you currently working a part-time job? Please enter 'yes' or 'no'\n")
+    # Change job_status based on input
+    if job_status == "yes":
+        job_status = True
+        part_time_income = input("What is your part-time income per month?\n")
+    else:
+        job_status = False
+
+    monthly_income = calculate_monthly_income(loan_amount, part_time_income, job_status)
+
+    print(f"Okay! Your monthly income is: £{monthly_income:.2f}")
+
+
+
+
